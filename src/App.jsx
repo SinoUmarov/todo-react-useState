6e0a183f9c9d986ae120fa7b5558237d3f4c50e8
SinoUmarov <usino62@email.com>
2025-05-16 16:22:40 +0500

    ff

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import React, { useState } from "react";
 import "./App.css";
+import { Button, Checkbox } from "@mui/material";
+import { Input } from "antd";
+import { AudioOutlined } from "@ant-design/icons";
+import { DeleteOutlined,EditOutlined,UserAddOutlined,SaveOutlined ,CheckCircleOutlined} from '@ant-design/icons';
+const { Search } = Input;
+
 const App = () => {
   const [users, setUsers] = useState([
     { id: 1, name: "Ali", age: 12, complete: true },
@@ -18,15 +24,15 @@ const App = () => {
     }));
   };
 
-  function handleCheck(user) {
+  const handleCheck = (user) => {
     const updatedUsers = users.map((el) =>
       el.id === user.id ? { ...el, complete: !el.complete } : el
     );
     setUsers(updatedUsers);
-  }
+  };
 
   const handleAdd = () => {
-    if (form.name.trim() === "" || form.age.trim() === "") return;
+    if (form.name.trim() === "" || form.age.trim() === "" || isNaN(form.age)) return;
     const newUser = {
       id: Date.now(),
       name: form.name,
@@ -67,63 +73,83 @@ const App = () => {
     setUsers(users.filter((user) => user.id !== id));
   };
 
+  const suffix = (
+    <AudioOutlined
+      style={{
+        fontSize: 16,
+        color: "#1677ff",
+      }}
+    />
+  );
+
+  const onSearch = (value) => {
+    setSearchName(value);
+  };
+
   return (
     <div className="todo-div">
-     <div className="input-todo">
-       <input
-        type="text"
-        value={searchName}
-        onChange={(e) => setSearchName(e.target.value)}
-        placeholder="search by name"
-      />
+      <div className="input-todo">
+        <Search
+          placeholder="Search by name"
+          onSearch={onSearch}
+          value={searchName}
+          onChange={(e) => setSearchName(e.target.value)}
+          enterButton
+          suffix={suffix}
+        />
 
-      <input
-        type="text"
-        name="name"
-        value={form.name}
-        onChange={handleChange}
-      />
-      <input
-        type="number"
-        name="age"
-        value={form.age}
-        onChange={handleChange}
-      />
-      <label>
         <input
-          type="checkbox"
-          name="complete"
-          checked={form.complete}
+          type="text"
+          name="name"
+          placeholder="Name"
+          value={form.name}
           onChange={handleChange}
         />
-      </label>
-      <br />
-      {editingUserId ? (
-        <button onClick={handleSave} className="save">save</button>
-      ) : (
-        <button onClick={handleAdd} className="add-todo">add</button>
-      )}
+        <input
+          type="number"
+          name="age"
+          placeholder="Age"
+          value={form.age}
+          onChange={handleChange}
+        />
+        <label>
+          <Checkbox
+            name="complete"
+            checked={form.complete}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        {editingUserId ? (
+          <Button variant="contained" color="success" onClick={handleSave}>
+            <SaveOutlined />
+          </Button>
+        ) : (
+          <Button variant="contained" color="success" onClick={handleAdd}>
+            <UserAddOutlined />
+          </Button>
+        )}
+      </div>
 
-     </div>
       {users
         .filter((user) =>
           user.name.toLowerCase().includes(searchName.toLowerCase())
         )
         .map((user) => (
           <div key={user.id} className="div-todo">
-            <p> {user.name}</p>
-            <p> {user.age}</p>
-            <p> {user.complete ? "true" : "false"}</p>
+            <p>{user.name}</p>
+            <p>{user.age}</p>
+            <p>{user.complete ? "true" : "false"}</p>
             <div className="buttons-todo">
-              <button onClick={() => handleEdit(user)}>edit</button>
-              <button onClick={() => handleDelete(user.id)}>delete</button>
-              <button
-                onClick={() => {
-                  handleCheck(user);
-                }}
-              >
-                checked
-              </button>
+              <Button variant="contained" onClick={() => handleEdit(user)}>
+                <EditOutlined />
+              </Button>
+              <Button variant="contained" onClick={() => handleDelete(user.id)}>
+                <DeleteOutlined />
+              </Button>
+              <Button variant="contained" onClick={() => handleCheck(user)}>
+                <CheckCircleOutlined />
+              </Button>
             </div>
           </div>
         ))}
